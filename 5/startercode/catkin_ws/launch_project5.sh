5d80668c05193affab4abc62d61099e1aa2a32ae
K5200 <kss223@cornell.edu>
2022-12-25 02:36:25 -0800

    updated project 5 launch shell script to output only command text

--- 5/startercode/catkin_ws/launch_project5.sh
+++ 5/startercode/catkin_ws/launch_project5.sh
@@ -1,5 +1,2 @@
-source devel/setup.bash
-roslaunch motion_planning mp.launch &
-rosrun motion_planning marker_control.py &
-rosrun motion_planning motion_planning.py &
+echo -e 'run the following commands one by one in bash:\nsource devel/setup.bash\nroslaunch motion_planning mp.launch &\nrosrun motion_planning marker_control.py &\nrosrun motion_planning motion_planning.py &\nrosrun rviz rviz\n\nuse kill $(jobs -p) to exit background jobs'
 
